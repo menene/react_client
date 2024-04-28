2ff940c14a6e671c6c33306d6cc3a20d778f3a29
Diego <diegopablo012@gmail.com>
2024-04-28 12:16:07 -0600

    fix(Nav): resolve navigation bug where URL changed but new content did not render. Conflict with <a> tag href attribute resolved.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -15,7 +15,7 @@ const Nav = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#/">
+                <a className="navbar-brand">
                     <img src={Logo} alt="Logo" />
                 </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -24,12 +24,12 @@ const Nav = () => {
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className={page == "/" ? "nav-link active" : "nav-link"} onClick={() => navigate('/')} href="#/">
+                            <a className={page == "/" ? "nav-link active" : "nav-link"} onClick={() => navigate('/')}>
                                 <i className="fa-solid fa-house-chimney"></i> Inicio
                             </a>
                         </li>
                         <li className="nav-item">
-                            <a className={page == "/about" ? "nav-link active" : "nav-link"} href="#/about" onClick={() => navigate('/about')}>
+                            <a className={page == "/about" ? "nav-link active" : "nav-link"} onClick={() => navigate('/about')}>
                                 <i className="fa-solid fa-circle-info"></i> Sobre nosotros
                             </a>
                         </li>
@@ -37,19 +37,19 @@ const Nav = () => {
                             isLoggedIn ? (
                                 <>
                                     <li className="nav-item">
-                                        <a className={page == "/report" ? "nav-link active" : "nav-link"} href="#/report" onClick={() => navigate('/report')}>
+                                        <a className={page == "/report" ? "nav-link active" : "nav-link"} onClick={() => navigate('/report')}>
                                             <i className="fa-solid fa-chart-line"></i> Reporte
                                         </a>
                                     </li>
                                     <li className="nav-item">
-                                        <a className={page == "/logout" ? "nav-link active" : "nav-link"} href="#/logout" onClick={() => navigate('/logout')}>
+                                        <a className={page == "/logout" ? "nav-link active" : "nav-link"}  onClick={() => navigate('/logout')}>
                                             <i className="fa-solid fa-right-from-bracket"></i> Salir
                                         </a>
                                     </li>
                                 </>
                             ) : (
                                 <li className="nav-item">
-                                    <a className={page == "/login" ? "nav-link active" : "nav-link"} href="#/login" onClick={() => navigate('/login')}>
+                                    <a className={page == "/login" ? "nav-link active" : "nav-link"}  onClick={() => navigate('/login')}>
                                         <i className="fa-solid fa-right-to-bracket"></i> Ingresar
                                     </a>
                                 </li>
